2ffe91f19478867152c4349bfb5b9b7aa888ba2d
911484 <911484@LTPH023521.cts.com>
2025-04-21 20:07:03 +0800

    update method with Scanner class to take user input.

--- Java_Basics_selflearn/src/loops/ReverseNumber.java
+++ Java_Basics_selflearn/src/loops/ReverseNumber.java
@@ -1,9 +1,14 @@
 package loops;
 
+import java.util.Scanner;
+
 public class ReverseNumber {
 
 	public static void main(String[] args) {
-		int number = 1234, reverse = 0;
+		int number, reverse = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		number =sc.nextInt();
 		System.out.println("Original Number is : "+number);
 		while (number != 0) {
 			int temp = number%10 ;
